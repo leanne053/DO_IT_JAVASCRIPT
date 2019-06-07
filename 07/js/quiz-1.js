f88d48fc3f3abc3b7f6b2fdb3fae1b346512ad73
leanne053 <46750182+leanne053@users.noreply.github.com>
2019-06-07 22:46:36 +0900

    7과 array객체
    7과 array객체

--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -3,9 +3,31 @@
 
 		function showArray(arr) {
 			var str = "<table><tr>";
+			var total =0;
 			for (var i=0; i<arr.length; i++) {
 				str += "<td>" + arr[i] + "</td>";
+				total+=arr[i];
 			}
+			str +="<td>"+total+"</td>";
 			str += "</tr></table>";
 			document.write(str);
 		}
+		// 다른 방법
+		// var numbers = [2, 4, 6, 8, 10];
+		// showArray(numbers);
+		// var total =0;
+		// for(var i=0;i<numbers.length;i++){
+		// 		total +=numbers[i]; 
+		// }
+		//  numbers.push(total);
+		//  showArray(numbers);
+
+		// function showArray(arr) {
+		// 	var str = "<table><tr>";
+		// 	var total =0;
+		// 	for (var i=0; i<arr.length; i++) {
+		// 		str += "<td>" + arr[i] + "</td>";
+		// 	}
+		// 	str += "</tr></table>";
+		// 	document.write(str);
+		// }
---
+++ 07/js/quiz-2.js
@@ -0,0 +1,15 @@
+var quot = [];
+quot[0] = "겸손해라";
+quot[1] = "항상 배우고 익혀라";
+quot[2] = "앞을 보고 나아가라";
+quot[3] = "지금 보는 세상은 작은 공간에 불과하다";
+quot[4] = "항상 꿈꾸고 노력하라";
+quot[5] = "배려하는 생활을 하자";
+quot[6] = "내가 원하는 것은 남도 원한다";
+quot[7] = "가끔은 내 삶을 뒤돌아보자";
+quot[8] = "행복이란 내 마음 속에 있다";
+quot[9] = "모든 것은 타이밍이다.";
+
+var index = Math.floor(Math.random()*10);
+
+document.write("<p>&quot;"+quot[index]+"&quot;</p>");
