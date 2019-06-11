6b60ae0a7e989cff1f90d238474ed2855a947f8c
leanne053 <46750182+leanne053@users.noreply.github.com>
2019-06-11 13:10:13 +0900

    10과 BOM
    10과 BOM

---
+++ 10/js/popup.js
@@ -0,0 +1,6 @@
+function openPop(){
+    window.open("popup.html","","width=400, height=400");
+    if(newWin == null){
+        alert("팝업이 차단되어 있습니다. 팝업차단을 해제하고 새로고침을 해 주세요");
+    }
+}
---
+++ 10/js/quiz-1.js
@@ -0,0 +1,1 @@
+window.onload = window.open("current.html","","width=300, height=50");
